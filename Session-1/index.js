faa792b1232f6dab4e2e7ee978a8d2ad6f1acb44
minileisduk <leminhductod@gmail.com>
2020-09-19 21:17:20 +0700

    add session

---
+++ Session-1/index.js
@@ -0,0 +1,66 @@
+{
+    console.log ("Bài 3");
+    let message = "Coding is great";
+    console.log (message);
+    let studentCount = 0;
+    console.log (studentCount);
+}
+
+{
+    console.log ("Bài 4");
+    let message = "‘Coding might not be easy, but still great’,";
+    console.log (message);
+    let total of the students in our class right now = 16;
+    console.log (total of the students in our class right now);
+}
+
+{
+    console.log ("Bài 5");
+    alert ('You look beatiful to day');
+}
+
+{
+    console.log ("Bài 6");
+    let name = prompt ('Hi there, your name please?');
+    alert ('Hi ' + name);
+}
+
+{
+    console.log ("Bài 7");
+    let firstname = prompt ('Enter your first name');
+    let lastname = prompt ('Enter your last name');
+    alert ('Hi '+ firstname + lastname);
+}
+
+{
+    console.log ("Bài 8");
+    let a = prompt ('Enter side lenght of square');
+    let area = a*a;
+    alert ('The square area is' + area);
+}
+
+{
+    console.log ("Bài 9");
+    let c = prompt ('Enter the radius of the circle');
+    let area = c*c*3.14;
+    alert ('The circle area is' + area);
+}
+
+{
+    console.log ("Bài 10");
+    let a = prompt ('Enter the temperature in Celsius');
+    let b = 9/5a*a + 32;
+    alert ('The Fahrenheit is' + b);
+}
+
+
+
+
+
+
+
+
+
+
+
+
